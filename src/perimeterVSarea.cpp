b0b00ec0ce007d3897ffe036600c23f54336e011
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2015-04-26 11:15:20 +0200

    Code perimeterVSarea function.

--- src/perimeterVSarea.cpp
+++ src/perimeterVSarea.cpp
@@ -1,4 +1,9 @@
+#include <cmath>
 #include "perimeterVSarea.h"
+
+double dist(Point p1, Point p2) {
+    return sqrt(pow(p1[0]-p2[0], 2) + pow(p1[1]-p2[1], 2)) ;
+}
 
 vector<Point> pos = {Point(-1, -1), Point(-1, 0), Point(-1, 1), Point(0, 1), Point(1, 1), Point(1, 0), Point(1, -1), Point(0, -1)};
 
@@ -58,7 +63,21 @@ vector<Point> extractBorder(Domain domain, DigitalSet object) {
       return border ;
 }
 
-double perimeterVSarea(Domain domain, DigitalSet object) {
+double getPerimeter(Domain domain, DigitalSet object) {
     vector<Point> border = extractBorder(domain, object) ;
-    return 0 ;
+    double perimeter = dist(border[0], border[border.size()-1]);
+    for(unsigned i = 0 ; i < border.size()-1 ; i++) {
+        perimeter += dist(border[i], border[i+1]) ;
+    }
+    return perimeter ;
+}
+
+double getArea(DigitalSet object) {
+    return (double)object.size() ;
+}
+
+double perimeterVSarea(Domain domain, DigitalSet object) {
+    double perimeter = getPerimeter(domain, object) ;
+    double area = getArea(object) ;
+    return area/(M_PI*perimeter*perimeter) ;
 }
