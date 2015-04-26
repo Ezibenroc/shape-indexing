1aea5cdb00f3f3c9120bde4adc7c44d4c3043061
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2015-04-26 21:41:23 +0200

    Multithreading \o/

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <vector>
 #include <cassert>
+#include <thread>
+#include <mutex>
 #include <boost/filesystem.hpp>
 #include <DGtal/base/Common.h>
 #include <DGtal/io/boards/Board2D.h>
@@ -19,32 +21,66 @@ using namespace DGtal;
 using namespace DGtal::Z2i; //We'll only consider Z² digital space on
                 //32bit integers
 
-int main(int argc, char *argv[])
-{
-    if(argc < 3) {
-        cerr << "Syntax:" << argv[0] << " <image filename> <classes repositories>" << endl ;
+void computeClasses(int argc, char *argv[], int *index, mutex *index_mutex, vector<ImageClass> *classes, mutex *classes_mutex) {
+    bool flag = true ;
+    int my_index ;
+    while(flag) {
+        index_mutex->lock() ;
+        if(*index < argc) {
+            my_index = *index ;
+            *index = *index + 1 ;
+            cout << argv[my_index] << endl ;
+        }
+        else {
+            flag = false ;
+        }
+        index_mutex->unlock() ;
+        if(!flag)
+            break ;
+        vector<string> filenames ;
+        fs::path dir(argv[my_index]) ;
+        fs::directory_iterator end ;
+        for(fs::directory_iterator iter(dir) ; iter != end ; ++iter) {
+            assert(fs::is_regular_file(iter->status())) ;
+            filenames.push_back(iter->path().string());
+        }
+        ImageClass img(filenames) ;
+        classes_mutex->lock() ;
+        classes->push_back(img);
+        img.dump() ;
+        classes_mutex->unlock() ;
+        filenames.clear() ;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if(argc < 4) {
+        cerr << "Syntax:" << argv[0] << " <number of threads> <image filename> <classes repositories>" << endl ;
         return 1 ;
     }
-
-    vector<ImageClass> classes ;
-    // Check validity of directories
-    for(int i = 2 ; i < argc ; i++) {
+    int nbThreads = min(atoi(argv[1]), argc-2) ;
+    if(nbThreads <= 0) {
+        cerr << "Syntax:" << argv[0] << " <number of threads> <image filename> <classes repositories>" << endl ;
+        return 1 ;
+    }    // Check validity of directories
+    for(int i = 3 ; i < argc ; i++) {
         fs::path dir(argv[i]) ;
         if(!fs::exists(dir) || !fs::is_directory(dir)) {
             cerr << "Error: " << argv[i] << " is not a directory." << endl ;
             return 1 ;
         }
     }
-    for(int i = 2 ; i < argc ; i++) {
-        vector<string> filenames ;
-        fs::path dir(argv[i]) ;
-        fs::directory_iterator end ;
-        for(fs::directory_iterator iter(dir) ; iter != end ; ++iter) {
-            assert(fs::is_regular_file(iter->status())) ;
-            filenames.push_back(iter->path().string());
-        }
-        classes.push_back(ImageClass(filenames));
-        filenames.clear() ;
+    int index = 3 ;
+    mutex index_mutex ;
+    vector<ImageClass> classes ;
+    mutex classes_mutex ;
+    vector<thread> threads ;
+    for(int i = 0 ; i < nbThreads ; i++) {
+        threads.push_back(thread(computeClasses, argc, argv, &index, &index_mutex, &classes, &classes_mutex)) ;
+    }
+
+    for(int i = 0 ; i < nbThreads ; i++) {
+        threads[i].join() ;
     }
 
     string filename1 = argv[1] ;
@@ -54,7 +90,6 @@ int main(int argc, char *argv[])
         cout << "distance min = " << classes[i].minDistance(img1) << endl ;
         cout << "distance max = " << classes[i].maxDistance(img1) << endl ;
         cout << "mean distance = " << classes[i].meanDistance(img1) << endl << endl ;
-        classes[i].dump() ;
     }
     img1.dump() ;
     return 0;
