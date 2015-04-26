5150020c7a760bd368dbf6ca46b4914bbd0bba4a
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2015-04-26 15:55:12 +0200

    Computation of distance in main.

--- src/main.cpp
+++ src/main.cpp
@@ -17,16 +17,17 @@ using namespace DGtal::Z2i; //We'll only consider Z² digital space on
 
 int main(int argc, char *argv[])
 {
-    if(argc != 2) {
-        cerr << "Syntax:" << argv[0] << " <filename>" << endl ;
+    if(argc != 3) {
+        cerr << "Syntax:" << argv[0] << " <filename 1> <filename 2>" << endl ;
         return 1 ;
     }
-    std::string filename = argv[1];
+    string filename1 = argv[1];
+    string filename2 = argv[2];
     //Image type (image of unsigned int)
-
-    ImageCharacterization img = ImageCharacterization(filename) ;
-    vector<double> signature = img.getSignatureVector() ;
-    cout << signature[0] << endl ;
-    img.dump() ;
+    ImageCharacterization img1 = ImageCharacterization(filename1) ;
+    ImageCharacterization img2 = ImageCharacterization(filename2) ;
+    cout << "distance = " << img1.distance(img2) << endl ;
+    img1.dump() ;
+    img2.dump() ;
     return 0;
 }
