1cb093579dfa75bc5de3163c6448a252852f6b8b
Romain Labolle <romain.labolle@ens-lyon.org>
2015-04-29 18:41:50 +0200

    Tabs to whitespaces

--- src/filter.cpp
+++ src/filter.cpp
@@ -12,23 +12,23 @@ Image filterNoise(const Image& img) {
     for(unsigned int k=0;k<9;k++) {
         max += matrix[k];
     }
-	for (auto it = dom.begin(), itend = dom.end(); it != itend; ++it) {
-		Point p = *it;
+    for (auto it = dom.begin(), itend = dom.end(); it != itend; ++it) {
+        Point p = *it;
 
-		unsigned int value = 0;
-		for(int i=-1; i<= 1; i++) {
-    		for(int j=-1; j<= 1; j++) {
-    		    Point q = p + Point(i,j);
-    		    if(dom.isInside(q) && img(q) > 0) {
-    		        value += matrix[(i+1)*3+(j+1)];
-    		    }
-    		}
+        unsigned int value = 0;
+        for(int i=-1; i<= 1; i++) {
+            for(int j=-1; j<= 1; j++) {
+                Point q = p + Point(i,j);
+                if(dom.isInside(q) && img(q) > 0) {
+                    value += matrix[(i+1)*3+(j+1)];
+                }
+            }
         }
-		
-		if (value >= max/2)
-			out.setValue(p,1);
-		else
-			out.setValue(p,0);
-	}
-	return out;
+
+        if (value >= max/2)
+            out.setValue(p,1);
+        else
+            out.setValue(p,0);
+    }
+    return out;
 }
